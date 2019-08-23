69f833a9633c2e746274aacc4fe57e02ffa96d25
shl <shl_glo@sina.com>
2019-08-23 11:38:32 +0800

    spring-boot-01

---
+++ demo1/src/main/java/com/sprintboottrain/readingList1/ReadingList1Application.java
@@ -0,0 +1,13 @@
+package com.sprintboottrain.readingList1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingList1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingList1Application.class, args);
+    }
+
+}
