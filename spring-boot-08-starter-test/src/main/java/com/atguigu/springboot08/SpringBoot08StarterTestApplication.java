93922be85aca718475f21bac205bd374c20cb826
shl <shl_glo@sina.com>
2019-08-23 13:20:29 +0800

    change already exist file

---
+++ spring-boot-08-starter-test/src/main/java/com/atguigu/springboot08/SpringBoot08StarterTestApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.springboot08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot08StarterTestApplication {
+	// edit by master for conflict
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot08StarterTestApplication.class, args);
+    }
+
+}
---
+++ spring-boot-08-starter-test/src/main/java/com/atguigu/springboot08/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.atguigu.springboot08.controller;
+
+import com.atguigu.starter.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/hello")
+    public String hello () {
+        return helloService.sayHellAtguigu("123");
+    }
+}
