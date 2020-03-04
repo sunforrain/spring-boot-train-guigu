d3f75bf5c35703449ef691ec45c7c4122baf307d
sunhl <sunhl@qq.com>
2020-03-04 11:45:44 +0800

    重新编写web相关的内容

--- spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/SpringBoot04WebRestfulcrudApplication.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/SpringBoot04WebRestfulcrudApplication.java
@@ -8,22 +8,34 @@ import org.springframework.web.servlet.ViewResolver;
 
 import java.util.Locale;
 
+/**
+ * 视频29 尚硅谷_SpringBoot_web开发-webjars&静态资源映射规则
+ *     有关mvc的可配置项可见WebMvcAutoConfiguration.java和WebMvcProperties.java
+ *     静态资源映射见 addResourceHandlers()方法
+ *     首页映射见 welcomePageHandlerMapping()方法
+ *     配置喜欢的图标见 FaviconConfiguration()方法
+ *
+ *  视频30 尚硅谷_SpringBoot_web开发-引入thymeleaf
+ *      因为springBoot打包为jar,内置tomcat的特性,导致无法使用jsp,需要使用其他的模板引擎
+ *      springBoot推荐thymeleaf
+ */
+
 @SpringBootApplication
 public class SpringBoot04WebRestfulcrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
     }
-    @Bean
-    public ViewResolver myViewResolver() {
-        return new myViewResolver();
-    }
-
-    private static class myViewResolver implements ViewResolver {
-
-        @Override
-        public View resolveViewName(String viewName, Locale locale) throws Exception {
-            return null;
-        }
-    }
+//    @Bean
+//    public ViewResolver myViewResolver() {
+//        return new myViewResolver();
+//    }
+//
+//    private static class myViewResolver implements ViewResolver {
+//
+//        @Override
+//        public View resolveViewName(String viewName, Locale locale) throws Exception {
+//            return null;
+//        }
+//    }
 }
--- spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.atguigu.springboot.controller;
 
-import com.atguigu.springboot.exception.UserNotExistException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,25 +11,43 @@ import java.util.Map;
 @Controller
 public class HelloController {
 
-//    @RequestMapping({"/","/login.html"})
+    //    @RequestMapping({"/","/login.html"})
 //    public String index(){
 //        return "index";
 //    }
+    // 视频29 尚硅谷_SpringBoot_web开发-webjars&静态资源映射规则
     @ResponseBody
     @RequestMapping("/hello")
-    public String hello (@RequestParam("user")String user) {
+    public String hello () {
+//        if(user.equals("aaa")){
+////            throw new UserNotExistException();
+////        }
+        return "hello world";
+    }
+
+    @ResponseBody
+    @RequestMapping("/hello2")
+    public String hello2 (@RequestParam("user")String user) {
         if(user.equals("aaa")){
-            throw new UserNotExistException();
+//            throw new UserNotExistException();
         }
         return "hello world";
     }
 
+    // 视频31 尚硅谷_SpringBoot_web开发-thymeleaf语法
+    // 查出一些数据,在页面展示
     @RequestMapping("/success")
     public String success (Map<String, Object> map) {
         // classpath:/templates/success.html
-        map.put("hello","<h1>你好</h1>");
-        map.put("users", Arrays.asList("zhangsan", "lisi", "wangwu"));
+        map.put("hello","你好");
         return "success";
     }
+
+//    @RequestMapping("/success")
+//    public String success2 (Map<String, Object> map) {
+//        // classpath:/templates/success.html
+//        map.put("hello","<h1>你好</h1>");
+//        map.put("users", Arrays.asList("zhangsan", "lisi", "wangwu"));
+//        return "success";
+//    }
 }
-// 夏艳华,李天辰
